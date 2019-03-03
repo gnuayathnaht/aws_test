5513858f7d5c4bb5c0118720da03ccf3c4e97fb9
gnuayathnaht <gnuayahtnaht>
2019-03-03 09:21:54 -0800

    first

---
+++ src/main/java/com/example/spring_helloword_test/SpringHellowordTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_helloword_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHellowordTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHellowordTestApplication.class, args);
+	}
+
+}
